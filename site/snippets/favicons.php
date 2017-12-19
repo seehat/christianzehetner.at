7c65dc045fe498e8bc631a3c7fac14e7c212f110
Christian Zehetner <mail@christianzehetner.at>
2017-12-19 16:45:22 +0100

    Added snacks favicons.

--- site/snippets/favicons.php
+++ site/snippets/favicons.php
@@ -4,7 +4,7 @@
     <link rel="icon" type="image/png" href="/files/favicons/favicon-32x32.png" sizes="32x32">
     <link rel="icon" type="image/png" href="/files/favicons/favicon-16x16.png" sizes="16x16">
     <link rel="manifest" href="/files/favicons/manifest.json">
-    <link rel="mask-icon" href="/files/favicons/safari-pinned-tab.svg" color="#aec829">
+    <link rel="mask-icon" href="/files/favicons/safari-pinned-tab.svg" color="#95ce4b">
     <link rel="shortcut icon" href="/files/favicons/favicon.ico">
     <meta name="msapplication-config" content="/files/favicons/browserconfig.xml">
-    <meta name="theme-color" content="#ffffff">
+    <meta name="theme-color" content="#95ce4b">
